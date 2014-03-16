6e3b9cfb9947fdc5f038e64b62976856e63ef9bc
G. Arends <galo@jpresult.nl>
2014-03-16 13:07:30 -0400

    bump version 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gdarends@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends "apt"
 depends "htop"
